18e4c573c70fb17d6aee5dadcb2ac4970ff34d0a
Leon Liang <lelian@microsoft.com>
2017-07-24 15:11:22 -0700

    who what when where yes

--- SatyaOfficeApp/index.js
+++ SatyaOfficeApp/index.js
@@ -14,13 +14,21 @@ console.log("Server running at http://localhost:%d", port);
 // Query the trigger relay API every 3 seconds.
 var seconds = 2;
 var interval = seconds * 1000;
+var userid = "dirt";
 setInterval(function() {
   	console.log("I am doing my 2 second check");
   	// Check the trigger relay API for a new signal.
   	var request = require("request");
-	request('http://www.google.com', function (error, response, body) {
+	request ('http://satya-relay.azurewebsites.net/api/trigger/list/' + userid, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-    		console.log(body)
-  		}
+			// Check if there is a positive response for some shit to go down.
+			console.log(body)
+			// If there is, then you're going to need to play a quote.
+			request ('http://Satya-quote.azurewebsites.net/api/quotes/get/{UserID}', function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					// Play the fokin quote.
+				}
+			});
+		}
 	});
 }, interval);
