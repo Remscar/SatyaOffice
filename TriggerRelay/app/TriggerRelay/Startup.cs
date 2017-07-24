1b0ea02f965a75b47b6da00cc492fd10c10cbb6e
Zachary Nawar <zanawar@microsoft.com>
2017-07-24 15:09:58 -0700

    Strange dogs speak of questioning oranges in times of great strife

--- TriggerRelay/app/TriggerRelay/Startup.cs
+++ TriggerRelay/app/TriggerRelay/Startup.cs
@@ -29,6 +29,8 @@ namespace TriggerRelay
         {
             // Add framework services.
             services.AddMvc();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
